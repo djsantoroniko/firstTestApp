7fe7b7f26afcee280906b08158d9e12958d913d6
Nico Santoro <djsantoroniko@yahoo.it>
2013-06-11 17:26:03 +0200

    Added all features related to camera

--- assets/www/js/camera.js
+++ assets/www/js/camera.js
@@ -3,20 +3,59 @@
 function takePicture(){
 
 	alert("take a picture");
+	
+	/*var popover = new CameraPopoverOptions(300,300,100,100,Camera.PopoverArrowDirection.ARROW_ANY);
+	var options = { quality: 50, destinationType: Camera.DestinationType.DATA_URL, popoverOptions : popover };
+	*/
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 25, 
+		destinationType: Camera.DestinationType.FILE_URI }); 
 
-	navigator.camera.getPicture(onSuccess, onFail, 
-	{
-		quality: 100,
-		destinationType: Camera.DestinationType.DATA_URL
-	});
-
-	function onSuccess(imageData){
+	function onSuccess(imageURI) {
+		console.log("SUCCESS_FROM CAMERA");
 		var image = document.getElementById('image');
-		image.src = "data:image/jpeg;base64," + imageData;
+		image.src = imageURI;	
 	}
-
-	function onFail(error){
-		alert("Failed because: " + error);
+	
+	function onFail(message) {
+		alert('Failed because: ' + message);
 	}
-
 }
+
+	function getPhoto() {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: 0,
+		correctOrientation: true,
+		allowEdit : true,
+		targetWidth: 500,
+		targetHeight: 500});
+    }
+	
+	function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+	
+	function onPhotoURISuccess(imageURI) {
+      console.log("SUCCESS_FROM LIBRARY");
+		var image = document.getElementById('image');
+		image.src = imageURI;	
+    }
+
+	$(function() {
+
+            $( "img#image" ).on( 'tap', tapHandler );
+			
+            function tapHandler( event ) {
+			
+			var className = $('img#image').attr('class');
+				
+				if(className == "normal"){
+					$(event.target).removeClass("normal");
+					$(event.target).addClass( "overlay" );
+				}else{
+					$(event.target).removeClass("overlay");
+					$(event.target).addClass( "normal" );
+				}
+			}
+      });
